7744bdbace0d93427e2752cc3751df566a0f7ea3
Buford Eeds <bufordeeds8@gmail.com>
2020-01-24 11:52:11 -0600

    added instructions on how to play

--- src/views/Homepage/Homepage.js
+++ src/views/Homepage/Homepage.js
@@ -12,7 +12,23 @@ export const Home = ({ user }) => {
         />
       </div> */}
       <div className={"text-center"}>
-        <h1>{user.id ? `Welcome back to illuminight, ${user.username}` : 'Welcome to illuminight'}</h1>
+        <h1>
+          {user.id
+            ? `Welcome back to illuminight, ${user.username}`
+            : "Welcome to illuminight"}
+        </h1>
+        <h2>
+          The rules are <em>simple</em>. The game? <em>Not so much.</em>
+        </h2>
+
+        <h3>
+          Click the tiles to illuminate them. Once all of the tiles are lit you win the game.
+        </h3>
+        <h3>Every second that goes by is a point against you.</h3>
+        <h3>And every click is 10 points against you.</h3>
+        <h3>Only the fastest and most efficient will top the leaderboard.</h3>
+        <h3> Easy enough right?</h3>
+
         {user.id ? (
           <Link to="/game">
             <button>Start Game</button>
