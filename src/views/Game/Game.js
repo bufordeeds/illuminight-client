385c60fea087009913ca8c5e1e54b320c8984c5a
Yining <yining.wang@utexas.com>
2020-02-19 17:14:43 -0600

    added break before start game button

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -1,65 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Button from '@material-ui/core/Button';
-import { TilesContainer } from '../../containers/Tiles';
-import './game.css';
+import { TilesContainer } from "../../containers/Tiles";
+import "./game.css";
 
 export class Game extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			clicks: 0,
-			gameComplete: false,
-			litEhStateArr: [...Array(5)].map((a) => [...Array(5)].map((n) => 0))
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      gameComplete: false,
+      litEhStateArr: [...Array(5)].map(a => [...Array(5)].map(n => 0))
+    };
+  }
 
-	incrementClicks = () => {
-		this.setState((previousState) => {
-			return {
-				clicks: previousState.clicks + 1
-			};
-		});
-	};
+  incrementClicks = () => {
+    this.setState(previousState => {
+      return {
+        clicks: previousState.clicks + 1
+      };
+    });
+  };
 
-	gameComplete = () => {
-		this.setState({ gameComplete: true });
-	};
+  gameComplete = () => {
+    this.setState({ gameComplete: true });
+  };
 
-	playAgain = () => {
-		let allDark = [...Array(5)].map((a) => [...Array(5)].map((n) => 0));
-		// console.log(allDark);
-		this.setState({
-			clicks: 0,
-			gameComplete: false,
-			litEhStateArr: allDark
-		});
-		this.props.resetTimer(); // console.log in Tiles.js what litEhStateArr is
-		// whatever that matches the tiles lit status in app - won't be reset
-	};
+  playAgain = () => {
+    let allDark = [...Array(5)].map(a => [...Array(5)].map(n => 0));
+    // console.log(allDark);
+    this.setState({
+      clicks: 0,
+      gameComplete: false,
+      litEhStateArr: allDark
+    });
+    this.props.resetTimer(); // console.log in Tiles.js what litEhStateArr is
+    // whatever that matches the tiles lit status in app - won't be reset
+  };
 
-	render() {
-		return (
-
-			<div className={'game-container'}>
-				<TilesContainer
-					cols={this.props.cols}
-					rows={this.props.rows}
-					stopTimer={this.props.stopTimer}
-					incrementClicks={this.incrementClicks}
-					user={this.props.user}
-					gameComplete={this.gameComplete}
-					time={this.props.time}
-					litEhStateArr={this.state.litEhStateArr}
-				/>
-				<span className={'clicks'}>Clicks: {this.state.clicks} </span>
-				<span className={'timer'}>
-					Timer:
-					{` ${this.props.time} s`}
-				</span>
-				<button onClick={this.playAgain}>Play Again</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={"game-container"}>
+        <TilesContainer
+          cols={this.props.cols}
+          rows={this.props.rows}
+          stopTimer={this.props.stopTimer}
+          incrementClicks={this.incrementClicks}
+          user={this.props.user}
+          gameComplete={this.gameComplete}
+          time={this.props.time}
+          litEhStateArr={this.state.litEhStateArr}
+        />
+        <span className={"clicks"}>Clicks: {this.state.clicks} </span>
+        <span className={"timer"}>
+          Timer:
+          {` ${this.props.time} s`}
+        </span>
+        <br></br>
+        <button onClick={this.playAgain}>Play Again</button>
+      </div>
+    );
+  }
 }
 
 export default Game;
